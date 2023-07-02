d07e98144eb9f87f7bb5508a2e2ca382678b6fd6
faried-elrewany <fariedezz4@gmail.com>
2023-07-03 01:40:01 +0300

    update cart design

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import clothe from "./images/clothe.png";
-import CouponInput from "./Coupon";
+import { Input } from "@material-tailwind/react";
 
-function ProductCard() {
+function ProductCard({
+  id,
+  name,
+  price,
+  quantity,
+  totalPrice,
+  onDelete,
+  onQuantityChange,
+}) {
+  const handleQuantityChange = (event) => {
+    const newQuantity = parseInt(event.target.value);
+    onQuantityChange(id, newQuantity);
+  };
+
   return (
     <div className="rounded-lg mb-3 bg-indigo-50 mx-auto lg:w-2/3">
       <div className="flex flex-col gap-4 justify-around p-3 items-center text-sm md:flex-row lg:text-lg">
@@ -13,7 +26,7 @@ function ProductCard() {
         {/* <!--  product description  box --> */}
         <div className="text-center mb-2">
           {/* <!-- product title --> */}
-          <h2 className="font-semibold">Winter Thick Hooded</h2>
+          <h2 className="font-semibold">{name}</h2>
           {/* <!-- product seller --> */}
           <p className="text-sm">
             sold by <i className="text-indigo-700">Faried Fouad Elrewany</i>
@@ -24,19 +37,22 @@ function ProductCard() {
         {/* <!--  product  price  --> */}
         <div className="text-center mb-2">
           <span className="text-2xl font-semibold">
-            23,453 <b className="text-slate-800 text-sm">$</b>
+            {price.toLocaleString()} <b className="text-slate-800 text-sm">$</b>
           </span>
           <span className="block text-indigo-700">60% OFF </span>
         </div>
       </div>
       {/* <!-- buttons and options  --> */}
       <div className="w-full flex justify-evenly pb-3 items-center">
-        <button className="bg-indigo-500 font-bold text-indigo-50 px-4 py-2 rounded-lg text-center transition-colors hover:bg-indigo-600">
+        <button
+          className="bg-indigo-500 font-bold text-indigo-50 px-4 py-2 rounded-lg text-center transition-colors hover:bg-indigo-600"
+          onClick={() => onDelete(id)}
+        >
           Delete
         </button>
         <div className="font-semibold text-xl">
           <div>
-            <label htmlFor="Quantity" className="sr-only">
+            <label htmlFor={`Quantity-${id}`} className="sr-only">
               Quantity
             </label>
 
@@ -44,20 +60,29 @@ function ProductCard() {
               <button
                 type="button"
                 className="w-10 h-10 text-3xl leading-10 text-indigo-600 transition hover:opacity-75"
+                onClick={() => {
+                  if (quantity > 1) {
+                    onQuantityChange(id, quantity - 1);
+                  }
+                }}
               >
                 -
               </button>
 
               <input
                 type="number"
-                id="Quantity"
-                value="1"
-                className="h-10 w-16 rounded outline-none border-indigo-200 text-center [-moz-appearance:_textfield] sm:text-sm [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none"
+                id={`Quantity-${id}`}
+                value={quantity}
+                className="h-10 w-16 rounded outline-none border-indigo-200 text-center [-moz-appearance:_textfield] sm:text-sm [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none text-black"
+                onChange={handleQuantityChange}
               />
 
               <button
                 type="button"
                 className="w-10 h-10 text-3xl leading-10 text-indigo-600 transition hover:opacity-75"
+                onClick={() => {
+                  onQuantityChange(id, quantity + 1);
+                }}
               >
                 +
               </button>
@@ -65,63 +90,73 @@ function ProductCard() {
           </div>
         </div>
       </div>
+      <div className="text-center font-semibold">
+        Total price: {totalPrice.toLocaleString()}{" "}
+        <b className="text-slate-800 text-sm">$</b>
+      </div>
     </div>
   );
 }
 
 const Cart = () => {
+  const [cart, setCart] = useState([
+    { id: 1, name: "Winter Thick Hooded", price: 23453, quantity: 1 },
+    { id: 2, name: "Summer Cotton T-shirt", price: 1500, quantity: 2 },
+    { id: 3, name: "Leather Boots", price: 7899, quantity: 1 },
+  ]);
+  const [totalPrice, setTotalPrice] = useState(
+    cart.reduce((total, item) => total + item.price * item.quantity, 0)
+  );
+
+  const handleDelete = (id) => {
+    const itemToRemove = cart.find((item) => item.id === id);
+    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+    setTotalPrice(
+      (prevTotalPrice) =>
+        prevTotalPrice - itemToRemove.price * itemToRemove.quantity
+    );
+  };
+
+  const handleQuantityChange = (id, newQuantity) => {
+    setCart((prevCart) =>
+      prevCart.map((item) =>
+        item.id === id ? { ...item, quantity: newQuantity } : item
+      )
+    );
+    setTotalPrice((prevTotalPrice) => {
+      const itemToUpdate = cart.find((item) => item.id === id);
+      const priceDiff =
+        itemToUpdate.price * (newQuantity - itemToUpdate.quantity);
+      return prevTotalPrice + priceDiff;
+    });
+  };
+
   return (
-    <>
-      <div className="mb-3 text-center mx-auto text-xl p-2 font-semibold bg-indigo-200 lg:w-2/3">
-        <i className="fa-solid fa-cart-shopping mr-2"></i>
-        Congrats you're eligible for a Coupon Code in this order
-      </div>
-
-      <div className="grid grid-cols-1 px-2">
-        <p className="mx-auto font-bold lg:w-2/3 mb-2">My Cart Items</p>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-      </div>
-
-      {/* <!-- checkout with coupons  --> */}
-      <div className="w-96 text-slate-700 font-semibold text-lg rounded-lg border-4 border-indigo-400 mb-4 mx-auto lg:w-2/3">
-        <div className="flex justify-around items-center py-3 bg-indigo-200">
-          <p>Amount</p>
-          <span className="text-2xl font-semibold">
-            250<b className="text-slate-800 text-sm">$</b>
-          </span>
+    <div className="py-8 px-4 sm:px-6 lg:px-8">
+      <h1 className="text-2xl font-bold mb-4">Shopping Cart</h1>
+      {cart.length === 0 ? (
+        <p className="text-lg mb-4">Your cart is empty</p>
+      ) : (
+        <div className="flex flex-col gap-4">
+          {cart.map((item) => (
+            <ProductCard
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              price={item.price}
+              quantity={item.quantity}
+              totalPrice={item.price * item.quantity}
+              onDelete={handleDelete}
+              onQuantityChange={handleQuantityChange}
+            />
+          ))}
+          <div className="text-center font-semibold">
+            Total price: {totalPrice.toLocaleString()}{" "}
+            <b className="text-slate-800 text-sm">$</b>
+          </div>
         </div>
-
-        <div className="flex justify-center py-3 px-9 items-center ">
-          <CouponInput />
-        </div>
-        <div className="flex justify-around border-t-2 border-slate-500 py-3 items-center bg-indigo-200">
-          <p className="text-2xl tracking-wide">Total Amount</p>
-          <span className="text-2xl font-semibold">
-            228<b className="text-slate-800 text-sm">$</b>
-          </span>
-        </div>
-
-        <div className="mt-4 text-center mb-3">
-          <button className="py-2 rounded px-3 transition-colors bg-indigo-400 hover:bg-indigo-600 text-white mx-auto w-2/3 text-sm md:text-lg">
-            CHECKOUT
-          </button>
-        </div>
-      </div>
-      {/* <!-- thank you box  --> */}
-      <div className="container flex items-center justify-around text-slate-700 font-semibold text-lg rounded-lg mb-4 mx-auto lg:w-2/3">
-        <p>
-          Thank you <b className="text-indigo-700">reka</b>
-        </p>
-        <p>
-          ORDER <span> #5624</span>
-        </p>
-      </div>
-    </>
+      )}
+    </div>
   );
 };
 
--- src/Coupon.jsx
+++ src/Coupon.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Input, Button } from "@material-tailwind/react";
 
-export default function CouponInput(props) {
-  const [coupon, setCoupon] = useState("");
-
+export default function CouponInput({ value, onChange, onClear }) {
   const handleCouponChange = (event) => {
-    setCoupon(event.target.value);
+    const newValue = event?.target?.value || "";
+    setCoupon(newValue);
   };
 
   const handleApplyClick = () => {
-    // TODO: Implement coupon apply feature
-    console.log(`Applying coupon code ${coupon}`);
-    // ابقي شوف هتطبق الكوبون ازاي
-    // props.onApply(coupon);
+    console.log(`Applying coupon code ${value}`);
+    // Call the onChange function to update the coupon state
+    onChange(value);
+  };
+
+  const handleClearClick = () => {
+    // Call the onClear function to clear the coupon state
+    onClear();
   };
 
   return (
@@ -20,7 +23,7 @@ export default function CouponInput(props) {
       <Input
         type="text"
         label="Coupon Code"
-        value={coupon}
+        value={value}
         onChange={handleCouponChange}
         className="pr-20"
         color="indigo"
@@ -30,13 +33,23 @@ export default function CouponInput(props) {
       />
       <Button
         size="sm"
-        color={coupon ? "indigo" : "blue-gray"}
-        disabled={!coupon}
+        color={value ? "indigo" : "blue-gray"}
+        disabled={!value}
         className="!absolute right-1 top-1 rounded"
         onClick={handleApplyClick}
       >
         Apply
       </Button>
+      {value && (
+        <Button
+          size="sm"
+          color="red"
+          className="!absolute right-20 top-1 rounded"
+          onClick={handleClearClick}
+        >
+          Clear
+        </Button>
+      )}
     </div>
   );
 }
