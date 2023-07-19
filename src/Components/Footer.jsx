7079ddd8b7e8ff1c55488cbbf96bf93766446c86
faried-elrewany <fariedezz4@gmail.com>
2023-07-19 23:10:27 +0300

    update website main icon and carousal auto play

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,13 @@
 import { Typography } from "@material-tailwind/react";
-
+import { Link } from "react-router-dom";
+import React from "react";
 export default function Footer() {
   return (
     <footer className="w-full bg-white p-8">
       <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-white text-center md:justify-between">
-        <img src="/img/logo-ct-dark.png" alt="logo-ct" className="w-10" />
+        <Link to="/">
+          <i className="fa-solid fa-f"></i>
+        </Link>
         <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
           <li>
             <Typography
@@ -50,7 +53,7 @@ export default function Footer() {
       </div>
       <hr className="my-8 border-blue-gray-50" />
       <Typography color="blue-gray" className="text-center font-normal">
-        &copy; 2023 Material Tailwind
+        &copy; Faried Elrewany
       </Typography>
     </footer>
   );
